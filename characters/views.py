19b051d410bb8a1d8e5068c685cab3a137927a9f
Anaka Norfleet <fleetster22@gmail.com>
2023-06-15 10:47:33 -0400

    added 2 character views and base html

--- characters/views.py
+++ characters/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import redirect, render, get_object_or_404, get_list_or_404
 
 from marvel import Marvel
-from keys import PRIVATE_KEY, PUBLIC_KEY
 
 from .models import Characters
 
 
-def show_character(request, id):
-    character = Characters.objects.get(id=id)
+def list_characters(request):
+    char_list = get_list_or_404(Characters)
+    context = {"char_list": char_list}
+    return render(request, "characters/list.html", context)
+
+
+def show_character(request, name):
+    character = Characters.objects.get(name=name)
     context = {"character": character}
-    return render(request, character.html, context)
+    return render(request, "characters/character.html", context)
